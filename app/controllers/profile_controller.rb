418926ed268fe28a6685a7866e52411e9e8d1d41
nndd91 <nndd91@hotmail.com>
2017-05-24 15:31:56 +0800

    Slight update to profile

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -1,13 +1,11 @@
 class ProfileController < ApplicationController
-  
+
+# User needs to login before doing the following actions
+  before_action :authenticate_user!, only: [:index]
+
   def index
     @topics = Topic.where(user_id: current_user.id)
     @comments = Comment.where(user_id: current_user.id)
   end
-
-  def edit
-    @profile = current_user
-  end
-
 
 end
