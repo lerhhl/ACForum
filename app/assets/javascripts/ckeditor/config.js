ac5e5b61cd6cecc8ff01d2e1178cd7ba0ddbc8bd
nndd91 <nndd91@hotmail.com>
2017-05-25 00:38:55 +0800

    Update new topic view,add deployment support for ckeditor

--- app/assets/javascripts/ckeditor/config.js
+++ app/assets/javascripts/ckeditor/config.js
@@ -8,12 +8,10 @@ CKEDITOR.editorConfig = function (config) {
   config.toolbar = "simple";
 
   config.toolbar = [
-   ['Styles','Format','Font','FontSize'],
+   ['Styles','Format','Bold','Italic','Underline','StrikeThrough','-','Undo','Redo','-','Cut','Copy','Paste','Find','Replace','-','Outdent','Indent','-'],
    '/',
-   ['Bold','Italic','Underline','StrikeThrough','-','Undo','Redo','-','Cut','Copy','Paste','Find','Replace','-','Outdent','Indent','-'],
-   '/',
-   ['NumberedList','BulletedList','-','JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock'],
-   ['Table','-','Link','Smiley','TextColor']
+   ['Font','FontSize','NumberedList','BulletedList','-','JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock'],
+   ['Table','-','Link','Smiley','TextColor', 'Source']
 ] ;
 
   // ... rest of the original config.js  ...
